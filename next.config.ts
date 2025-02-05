890631da6cf6cdc72513851c17c3edeb4315d846
songa <thddk0121@gmail.com>
2025-02-06 01:42:05 +0900

    chore: @svgr/webpack 설치 및 SVG 처리 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     forceSwcTransforms: true
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
   }
 };
 
