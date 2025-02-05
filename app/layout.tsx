c4ad510795f703a562018ac73fe316fb6a93d4b0
songa <thddk0121@gmail.com>
2025-02-06 02:22:18 +0900

    chore: 프리텐다드 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
+import localFont from 'next/font/local';
+
 import type { Metadata } from 'next';
+
 import './globals.css';
+
+const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard'
+});
 
 export const metadata: Metadata = {
   title: '🍅 미루지마',
@@ -14,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="antialiased">{children}</body>
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
     </html>
   );
 }
