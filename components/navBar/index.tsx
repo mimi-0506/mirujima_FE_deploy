8f0218fe101be848f79e820418bf9901e7a3912b
mimi <0506phm@gmail.com>
2025-02-06 11:18:37 +0900

    feat: 사이드바 열고닫기

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -1,3 +1,28 @@
+'use client';
+import { useState } from 'react';
+
 export default function NavBar() {
-  return <div></div>;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const CloseButton = () => {
+    return (
+      <button
+        onClick={() => {
+          setIsOpen((x) => !x);
+        }}
+      >
+        닫기
+      </button>
+    );
+  };
+
+  return isOpen ? (
+    <div className="absolute left-0 flex h-screen w-[50px] flex-col border border-black">
+      <CloseButton />
+    </div>
+  ) : (
+    <div className="absolute left-0 flex h-screen w-[300px] flex-col border border-black">
+      <CloseButton />
+    </div>
+  );
 }
