a65224d7b3daa0163d191b436f10708d0d9ee353
mimi <0506phm@gmail.com>
2025-02-06 11:14:26 +0900

    feat: 루트 페이지에 Navbar추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import NavBar from '@/components/navBar';
+
 import Logo from '../public/images/logo/mirujima-logo.svg';
 
 export default function Home() {
   return (
     <div className="grid min-h-screen items-center justify-items-center">
+      <NavBar />
       <Logo />
     </div>
   );
