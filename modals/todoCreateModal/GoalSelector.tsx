5bf1221a6b5f1b980ac634e6ad67c84978bfb04b
mimi <0506phm@gmail.com>
2025-02-07 15:18:06 +0900

    refactor: props관련 경고 부분 리팩토링

--- modals/todoCreateModal/GoalSelector.tsx
+++ modals/todoCreateModal/GoalSelector.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import type { ChangeEvent } from 'react';
+
+import type { DebouncedFunc } from 'lodash';
 
 export default function GoalSelector({
   handleChangeIsValid
 }: {
-  handleChangeIsValid: (event: React.FormEvent) => void;
+  handleChangeIsValid: DebouncedFunc<() => void>;
 }) {
-  const [goalList, setGoalList] = useState<string[]>(['test1', 'test2', 'test3']);
+  const [goalList, setGoalList] = useState<string[]>([]);
+  const [selectedGoal, setSelectedGoal] = useState('');
+
+  useLayoutEffect(() => {
+    //GoalList 받아오는 로직 추가
+    setGoalList(['test1', 'test2', 'test3']);
+  }, []);
+
+  useEffect(() => {
+    handleChangeIsValid();
+  }, [selectedGoal, handleChangeIsValid]);
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedGoal(event.target.value);
+  };
 
   return (
     <div>
@@ -14,13 +31,14 @@ export default function GoalSelector({
         id="goal-select"
         className="rounded-md border p-2"
         name="goal"
-        onChange={handleChangeIsValid}
+        onChange={handleChange}
+        value={selectedGoal}
       >
-        <option defaultValue="" className="hidden" disabled selected>
+        <option value="" className="hidden" disabled>
           목표를 선택해주세요
         </option>
         {goalList.map((goal, index) => (
-          <option key={index} defaultValue={goal}>
+          <option key={index} value={goal}>
             {goal}
           </option>
         ))}
