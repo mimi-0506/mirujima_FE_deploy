9efe0195ddcb3b0466d4dfbf12bea51936ef6760
mimi <0506phm@gmail.com>
2025-02-10 17:11:53 +0900

    style: 애니메이션 및 css 적용

--- components/navBar/newTodo.tsx
+++ components/navBar/newTodo.tsx
@@ -2,16 +2,19 @@
 
 import { useModalStore } from '@/provider/store-provider';
 
+import AddIcon from '../../public/icon/add.svg';
+
 export default function NewTodo() {
   const { setIsTodoCreateModalOpen } = useModalStore((state) => state);
   return (
     <>
       <button
+        className="flex h-[46px] w-full items-center justify-center gap-2 rounded-[8px] bg-main text-white"
         onClick={() => {
           setIsTodoCreateModalOpen(true);
         }}
       >
-        새 할일
+        <AddIcon />새 할일
       </button>
     </>
   );
