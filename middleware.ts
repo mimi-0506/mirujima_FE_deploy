725469e5c7464257abd041625e9a4b4ad854cd0e
mimi <0506phm@gmail.com>
2025-02-10 10:28:24 +0900

    feat: 토큰 관련 미들웨어 라우팅 추가

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const accessToken = req.cookies.get('accessToken');
+
+  // 정적 파일 및 API 요청 무시
+  if (pathname.startsWith('/_next') || pathname.startsWith('/api')) {
+    return NextResponse.next();
+  }
+
+  if (pathname === '/') return NextResponse.redirect(new URL('/dashboard', req.url));
+
+  //로그인 완성되면 주석 풀기
+  //   if (accessToken) {
+  //     console.log(req.url, '액세스토큰 있음');
+  //     if (pathname === '/login' || pathname === '/signup')
+  //       return NextResponse.redirect(new URL('/dashboard', req.url));
+  //     else return NextResponse.next();
+  //   } else {
+  //     console.log(req.url, '액세스토큰 없음');
+  //     if (pathname === '/login' || pathname === '/signup') return NextResponse.next();
+  //     else return NextResponse.redirect(new URL('/login', req.url));
+  //   }
+}
+
+export const config = {
+  matcher: '/((?!_next|api).*)'
+};
