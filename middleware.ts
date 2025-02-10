c45de098681f9bc7b6e0da01dc149fe0bc3fa147
mimi <0506phm@gmail.com>
2025-02-10 13:35:58 +0900

    chore: 중복 제거

--- middleware.ts
+++ middleware.ts
@@ -20,7 +20,7 @@ export function middleware(req: NextRequest) {
     });
     //클라이언트 라이브러리라 서버에서 작동 불가. 다른 방법 찾아봐야 할듯
     //toast.success('로그아웃 되었습니다!', { duration: 2000 });
-    return NextResponse.redirect(new URL('/login', req.url));
+    return response;
   }
 
   // 우리쪽 api로 돌린 후 테스트
