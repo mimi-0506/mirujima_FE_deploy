17d0a540fb1cc152d4a18bb97944e5650152964e
mimi <0506phm@gmail.com>
2025-02-10 11:19:33 +0900

    chore: 주석 추가 및 수정

--- middleware.ts
+++ middleware.ts
@@ -16,11 +16,12 @@ export function middleware(req: NextRequest) {
   if (pathname === '/logout') {
     const cookiesToDelete = ['accessToken', 'refreshToken', 'user'];
     cookiesToDelete.forEach((cookie) => deleteCookie(cookie, { path: '/' }));
+    //클라이언트 라이브러리라 서버에서 작동 불가. 다른 방법 찾아봐야 할듯
     //toast.success('로그아웃 되었습니다!', { duration: 2000 });
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  //로그인 완성되면 주석 풀기
+  // 우리쪽 api로 돌린 후 테스트
   //   if (accessToken) {
   //     console.log(req.url, '액세스토큰 있음');
   //     if (pathname === '/login' || pathname === '/signup')
