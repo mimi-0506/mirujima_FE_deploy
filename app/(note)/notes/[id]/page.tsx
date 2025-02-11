986a7b4900295650ed8ed8ccd209020296eaa714
mimi <0506phm@gmail.com>
2025-02-11 09:15:35 +0900

    chore: 버셀 배포용 문제 발생 페이지 삭제

--- app/(note)/notes/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NoteDetail() {
-  return (
-    <main>
-      <div>NoteDetail</div>
-    </main>
-  );
-}
--- app/goals/[id]/pages.tsx
+++
@@ -1,3 +0,0 @@
-export default function GoalDetail() {
-  return <div></div>;
-}
