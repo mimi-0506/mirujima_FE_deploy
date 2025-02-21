c220749d575208943c9372a8b2f1e0cf40dd8d89
김지윤 <138554290+kirnjiyun@users.noreply.github.com>
2025-02-21 21:44:27 +0900

    feat: 목표 제목 부분 길이 제한 및 버튼 추가

--- app/dashboard/_components/GoalItem.tsx
+++ app/dashboard/_components/GoalItem.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import { useGetGoalDetail } from '@/hooks/goalsDetail/useGetGoalDetail';
 import TaskList from '@/components/TaskList/TaskList';
+import { useGetGoalDetail } from '@/hooks/goalsDetail/useGetGoalDetail';
+import { useInfoStore, useModalStore } from '@/provider/store-provider';
+import PlusIcon from '@/public/icon/plus-border-none.svg';
 import type { TodoType } from '@/types/todo.type';
 
 interface GoalItemProps {
@@ -12,7 +14,7 @@ interface GoalItemProps {
 export default function GoalItem({ goalId, title }: GoalItemProps) {
   // 1) goalId로 상세 정보( todos ) 받아오기
   const { data, isLoading, isError } = useGetGoalDetail(goalId.toString());
-
+  const { setIsTodoCreateModalOpen } = useModalStore((state) => state);
   if (isLoading) {
     return (
       <div className="rounded-container w-full p-4">
@@ -37,7 +39,18 @@ export default function GoalItem({ goalId, title }: GoalItemProps) {
 
   return (
     <div className="rounded-container w-full p-6">
-      <h3 className="text-lg font-bold">{title}</h3>
+      <div className="flex justify-between">
+        <h3 className="truncate text-lg font-bold">{title}</h3>{' '}
+        <button
+          onClick={() => {
+            setIsTodoCreateModalOpen(true);
+          }}
+          className="flex flex-shrink-0 items-center pl-1 text-main"
+        >
+          <PlusIcon /> 할일 추가
+        </button>
+      </div>
+
       <div className="my-6 h-2 w-full bg-slate-200"></div>
       <div className="mt-3 flex flex-col border-none desktop:flex-row">
         <div className="flex-1 overflow-y-auto">
