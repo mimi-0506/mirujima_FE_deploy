650078edbb478c20067060b20b46f55d752fb500
김지윤 <138554290+kirnjiyun@users.noreply.github.com>
2025-02-21 17:26:04 +0900

    feat: 구글 로그인 관련 라이브러리 설치

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,15 +2,14 @@
 
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-
+import { useGoogleLogin } from '@react-oauth/google';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
-import Image from 'next/image';
+import KaKaoIcon from '@/public/images/sns/kakao-icon.svg';
+import GoogleIcon from '@/public/images/sns/google-icon.svg';
 import { useRouter } from 'next/navigation';
 import { z } from 'zod';
-
 import { useInfoStore } from '@/provider/store-provider';
-
 import { useLoginMutation } from '../../../hooks/auth/useLoginMutation';
 import Button from '../_components/Button';
 import InputField from '../_components/InputField';
@@ -49,11 +48,13 @@ export default function LoginPage() {
     isError && axios.isAxiosError(error) && error.response?.data?.message
       ? error.response.data.message
       : null;
-
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse)
+  });
   return (
     <>
       <h1 className="mb-[60px] text-[26px] font-semibold leading-[28px] md:text-[34px] md:leading-[41px]">
-        로그인
+        로그인{' '}
       </h1>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
         <InputField
@@ -92,33 +93,22 @@ export default function LoginPage() {
           회원가입
         </Button>
       </form>
-      <div className="mt-[40px] hidden gap-4">
+      <div className="mt-[40px] gap-4">
         <label className="font-semibold text-gray500">간편 로그인</label>
         <div className="flex flex-col gap-3">
           <Button
+            onClick={() => login()}
             type="button"
-            className="flex cursor-not-allowed items-center justify-center gap-2 border border-gray-300 bg-white px-4 py-2 text-gray500"
+            className="flex items-center justify-center gap-2 border border-gray-300 bg-white px-4 py-2 text-gray500"
           >
-            <Image
-              src="/images/sns/google-icon.svg"
-              alt="구글 로고"
-              width={24}
-              height={24}
-              className="h-auto w-auto"
-            />
+            <GoogleIcon />
             <span className="text-[16px] font-semibold leading-[22px]">구글 계정으로 로그인</span>
           </Button>
           <Button
             type="button"
             className="flex cursor-not-allowed items-center justify-center gap-2 border border-gray-300 bg-white px-4 py-2 text-gray500"
           >
-            <Image
-              src="/images/sns/kakao-icon.svg"
-              alt="카카오 로고"
-              width={24}
-              height={24}
-              className="h-auto w-auto"
-            />
+            <KaKaoIcon alt="카카오 로고" />
             <span className="text-[16px] font-semibold leading-[22px]">카카오 계정으로 로그인</span>
           </Button>
         </div>
--- app/AllProviders.tsx
+++ app/AllProviders.tsx
@@ -5,31 +5,33 @@ import {
   ModalStoreProvider,
   TodoCreateModalStoreProvider
 } from '@/provider/store-provider';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import ModalArea from './ModalArea';
 import ReactQueryProvider from '../hooks/useReactQuery';
-
+const GOOGLE_CLIENT_ID = process.env.NEXT_GOOGLE_CLIENT_ID;
 export default function AllProviders({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <InfoStoreProvider>
-      <ModalStoreProvider>
-        <TodoCreateModalStoreProvider>
-          <ReactQueryProvider>
-            {children}
-            <ModalArea />
-            <Toaster
-              containerStyle={{
-                bottom: 100
-              }}
-            />
-            <div id="modal-portal" />
-          </ReactQueryProvider>
-        </TodoCreateModalStoreProvider>
-      </ModalStoreProvider>
-    </InfoStoreProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID ?? ''}>
+      <InfoStoreProvider>
+        <ModalStoreProvider>
+          <TodoCreateModalStoreProvider>
+            <ReactQueryProvider>
+              {children}
+              <ModalArea />
+              <Toaster
+                containerStyle={{
+                  bottom: 100
+                }}
+              />
+              <div id="modal-portal" />
+            </ReactQueryProvider>
+          </TodoCreateModalStoreProvider>
+        </ModalStoreProvider>
+      </InfoStoreProvider>
+    </GoogleOAuthProvider>
   );
 }
