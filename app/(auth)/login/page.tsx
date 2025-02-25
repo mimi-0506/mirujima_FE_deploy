bef384dac90fb9180faa5e6dd9356469a1d30b79
mimi <0506phm@gmail.com>
2025-02-25 11:29:38 +0900

    chore: 빌드용 경고제거주석, 린트 에러 수정

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -37,6 +37,7 @@ export default function LoginPage() {
   useEffect(() => {
     setIsLoading(false);
     logout();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const { mutate: loginMutate, isError, error } = useLoginMutation();
