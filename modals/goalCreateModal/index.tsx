93785f70ca93e7f829d88b926074f1d306381535
mimi <0506phm@gmail.com>
2025-02-25 09:39:43 +0900

    feat: 목표생성시 완료 날짜 지정 추가

--- modals/goalCreateModal/index.tsx
+++ modals/goalCreateModal/index.tsx
@@ -13,39 +13,52 @@ export default function GoalCreateModal() {
   const { mutateAsync } = useSetNewGoal();
   const [valid, setValid] = useState(false);
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const textRef = useRef<HTMLInputElement>(null);
+  const dateRef = useRef<HTMLInputElement>(null);
 
   const handleValidCheck = useCallback(
-    debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+    debounce((e) => {
       setValid(e.target.value.trim() !== '');
     }, 50),
     []
   );
 
   const handleGoalCreat = async () => {
-    if (inputRef.current) {
-      const nowGoal = inputRef.current.value;
-      await mutateAsync(nowGoal);
+    if (textRef.current && dateRef.current) {
+      const nowGoal = textRef.current.value;
+      const endDate = dateRef.current.value;
+
+      await mutateAsync({ nowGoal, endDate });
       setIsGoalCreateModalOpen(false);
     }
   };
 
-  const handleClose = () => {
+  const handleClose = (e) => {
     setIsGoalCreateModalOpen(false);
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') handleGoalCreat();
+  };
+
   return (
-    <Overlay>
-      <div className="box-border flex w-[520px] flex-col rounded-xl bg-white p-6">
+    <Overlay onClick={handleClose}>
+      <div
+        className="box-border flex w-[520px] flex-col rounded-xl bg-white p-6"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="flex items-center justify-between text-gray500">
           <h1 className="text-[20px]">목표 생성</h1> <CloseButton handleClose={handleClose} />
         </div>
         <input
-          ref={inputRef}
+          ref={textRef}
           onChange={handleValidCheck}
           className="mb-[28px] mt-[25px] box-border h-[50px] w-full rounded-lg border border-gray200 px-[16px] py-[14px]"
           placeholder="목표를 적어주세요"
+          onKeyDown={handleKeyPress}
         />
+        완료날짜
+        <input type="date" ref={dateRef} />
         <button
           className={`${
             valid ? 'bg-main' : 'cursor-not-allowed bg-gray-400'
