8df557cabe56ff1e5164d4909279ee828101cc86
kirnjiyun <138554290+kirnjiyun@users.noreply.github.com>
2025-03-11 16:13:58 +0900

    feat: 콜백페이지

--- app/(auth)/auth/callback/page.tsx
+++ app/(auth)/auth/callback/page.tsx
@@ -4,7 +4,8 @@ import { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useGoogleLoginMutation } from '@/hooks/auth/useGoogleLoginMutation';
-
+import LoadingSpinner from '@/public/icon/spin.svg';
+import { GOOGLE_LOGIN_ERROR } from '@/constant/toastText';
 export default function GoogleCallbackPage() {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
@@ -21,20 +22,23 @@ export default function GoogleCallbackPage() {
       },
       onError: (err) => {
         console.error('구글 로그인 에러:', err);
-        toast.error('구글 로그인에 실패했습니다. 다시 시도해주세요.');
+        toast.error(GOOGLE_LOGIN_ERROR);
       }
     });
   }, [code, mutation, router]);
 
   return (
     <div>
-      {mutation.isPending
-        ? '구글 로그인 처리 중...'
-        : mutation.isError
-          ? '구글 로그인에 실패했습니다.'
-          : mutation.isSuccess
-            ? '로그인 성공! 이동 중...'
-            : '잠시만 기다려주세요...'}
+      {/* {mutation.isPending ? (
+        <LoadingSpinner width={48} height={48} />
+      ) : mutation.isError ? (
+        '구글 로그인에 실패했습니다.'
+      ) : mutation.isSuccess ? (
+        '로그인 성공! 이동 중...'
+      ) : (
+        <LoadingSpinner width={48} height={48} />
+      )} */}{' '}
+      <LoadingSpinner width={48} height={48} />
     </div>
   );
 }
