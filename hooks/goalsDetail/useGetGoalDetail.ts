bd92a34d877b96e98d7ec21384b53ad34752e526
kirnjiyun <138554290+kirnjiyun@users.noreply.github.com>
2025-03-14 17:21:09 +0900

    fix: 노트/123이면 뒤에 목표/123이 보이는 문제 해결

--- hooks/goalsDetail/useGetGoalDetail.ts
+++ hooks/goalsDetail/useGetGoalDetail.ts
@@ -2,7 +2,6 @@ import { useQuery } from '@tanstack/react-query';
 
 import { apiWithClientToken } from '@/apis/clientActions/index';
 import { useInfoStore } from '@/provider/store-provider';
-
 import type { ApiResponse } from '@/types/apiResponse.type';
 import type { GoalType } from '@/types/goal.types';
 import type { TodoType } from '@/types/todo.types';
@@ -20,10 +19,13 @@ const fetchGoalDetail = async (goalId: GoalType['id']): Promise<GoalDetailRespon
 
 export const useGetGoalDetail = (goalId: GoalType['id']) => {
   const userId = useInfoStore((state) => state.userId);
+
   const query = useQuery<GoalDetailResponse>({
     queryKey: ['goal', goalId, userId],
     queryFn: () => fetchGoalDetail(goalId),
     enabled: !!goalId
+    // staleTime: 5 * 60 * 1000,
+    // gcTime: 10 * 60 * 1000 // cacheTime → gcTime
   });
 
   return query;
