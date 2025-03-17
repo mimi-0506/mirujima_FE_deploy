a832525e53006df85d7d369a0da2cb7982845f0d
mimi <0506phm@gmail.com>
2025-03-17 17:05:59 +0900

    fix: 뽀모도로 타이머 시간 배포용으로 수정

--- constant/numbers.ts
+++ constant/numbers.ts
@@ -3,5 +3,5 @@ export const SMALL_MAX = 744;
 
 export const FILE_SIZE_5MB = 5 * 1024 * 1024;
 
-export const FOCUS_TIME = 10; // 25 * 60;
-export const BREAK_TIME = 5; // 5 * 60;
+export const FOCUS_TIME = 25 * 60;
+export const BREAK_TIME = 5 * 60;
